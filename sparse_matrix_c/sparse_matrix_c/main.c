3393df165c05680bd51813a45e7e579209d721ad
masintech <masintec@gmail.com>
2019-12-07 10:47:09 +0800

    sparse_matrix_c completed

--- sparse_matrix_c/sparse_matrix_c/main.c
+++ sparse_matrix_c/sparse_matrix_c/main.c
@@ -51,13 +51,58 @@ void display(struct Sparse s)
     }
 }
 
-add
+
+struct Sparse * add(struct Sparse *s1, struct Sparse *s2)
+{
+    struct Sparse *sum;
+    int i,j,k;
+    i=j=k=0;
+    sum = (struct Sparse *)malloc(sizeof(struct Sparse));
+    sum->ele = (struct Element *)malloc((s1->num+s2->num)*sizeof(struct Element));
+    
+    while(i<s1->num && j<s2->num){
+        if(s1->ele[i].i<s2->ele[j].i)
+            sum->ele[k++] = s1->ele[i++];
+        else if (s1->ele[i].i<s2->ele[j].i)
+            sum->ele[k++] = s2->ele[j++];
+        else
+        {
+            if(s1->ele[i].j<s2->ele[j].j)
+                sum->ele[k++] = s1->ele[i++];
+            else if (s1->ele[i].j<s2->ele[j].j)
+                sum->ele[k++] = s2->ele[j++];
+            else
+            {
+            sum->ele[k] = s1->ele[i];
+            sum->ele[k++].x = s1->ele[i++].x + s2->ele[j++].x;
+            }
+        }
+    }
+    for (;i<s1->num;i++) sum->ele[k++]=s1->ele[i];
+    for (;j<s2->num;j++) sum->ele[k++] =s2->ele[j];
+    sum->m = s1->m;
+    sum->n = s1->n;
+    sum->num = k;
+    return sum;
+    
+}
+
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    struct Sparse s;
-    create(&s);
-    display(s);
-        
+    struct Sparse s1, s2, *s3;
+    create(&s1);
+    create(&s2);
+
+    s3 = add(&s1, &s2);
+    printf("First Matrix\n");
+    display(s1);
+    printf("Second Matrix\n");
+    display(s2);
+    printf("Sum2 Matrix\n");
+    display(*s3);
+    
     
     return 0;
 }
